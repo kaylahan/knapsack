e8c3cbe8c38a6eb6918151069ad9a6df8798e76d
kaylahan <631189312@qq.com>
2021-01-13 16:09:46 +0800

    dev修改注释

--- src/GreedySolution.java
+++ src/GreedySolution.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class GreedySolution 
 {
     //  注释
-    //  新分支dev
+    //  新分支dev新修改
     public static void main(String [] args) throws Exception
     {
         DataProcessor.processData();
